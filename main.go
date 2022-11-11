40191329028a297647d747f955059b959321ecf4
Benjamin Tamasi <halftome@gmail.com>
2022-11-11 15:22:28 +0100

    Seperate out lib

--- main.go
+++ main.go
@@ -1,32 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"math"
 	"os"
-	"strconv"
-	"strings"
 )
 import "fmt"
 
 var precision = flag.Int("precision", 4, "max number of decimal places allowed in timecode")
 var outFile = flag.String("out", "", "Output file with fixed timecode")
-
-func almostEqual(a, b float64) bool {
-	return (1 / math.Pow10(*precision+1)) > math.Abs(a-b)
-}
-
-func gte(a, b float64) bool {
-	if !almostEqual(a, b) {
-		return a > b
-	}
-	return true
-}
-
-func floatStr(a float64) string {
-	return strconv.FormatFloat(a, 'f', *precision, 64)
-}
 
 func main() {
 	flag.Usage = func() {
@@ -39,7 +20,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	f, err := os.Open(flag.Arg(0))
+	in, err := os.Open(flag.Arg(0))
 
 	if err != nil {
 		fmt.Println(err)
@@ -48,21 +29,10 @@ func main() {
 
 	defer func(f *os.File) {
 		_ = f.Close()
-	}(f)
-
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
-
-	currentLineNumber := 0
-	var previousTimestamp float64
-	firstTimestamp := true
-
-	foundErrors := false
-
-	var writer *bufio.Writer
+	}(in)
 
 	if *outFile != "" {
-		f, err := os.Create(*outFile)
+		out, err := os.Create(*outFile)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -70,53 +40,21 @@ func main() {
 
 		defer func(f *os.File) {
 			_ = f.Close()
-		}(f)
+		}(out)
 
-		writer = bufio.NewWriter(f)
-		defer func(writer *bufio.Writer) {
-			_ = writer.Flush()
-		}(writer)
-	}
-
-	for scanner.Scan() {
-		l := scanner.Text()
-		if l != "" && !strings.HasPrefix(l, "#") {
-			if ts, err := strconv.ParseFloat(l, 64); err == nil {
-				if !firstTimestamp {
-					if gte(previousTimestamp, ts) {
-						fmt.Printf("%d: not monotone increasing: %s -> %s", currentLineNumber, floatStr(previousTimestamp), floatStr(ts))
-						foundErrors = true
-						if *outFile != "" {
-							fixedTimestamp := previousTimestamp + 1/math.Pow10(*precision)
-							fmt.Printf(" changing to %s", floatStr(fixedTimestamp))
-							ts = fixedTimestamp
-							l = floatStr(ts)
-						}
-						fmt.Println("")
-					}
-				}
-				firstTimestamp = false
-				previousTimestamp = ts
-			} else {
-				fmt.Printf("%d: invalid data: %s\n", currentLineNumber, l)
-				l = "## " + l
-			}
+		_, err = CleanTimecodeFile(in, out, *precision)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
 		}
-		if *outFile != "" {
-			_, err := writer.WriteString(l + "\n")
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
+	} else {
+		valid, err := CheckTimeCodeFile(in, *precision)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
 		}
-
-		currentLineNumber++
-	}
-
-	if *outFile != "" {
-	}
-
-	if foundErrors && *outFile == "" {
-		os.Exit(2)
+		if !valid {
+			os.Exit(2)
+		}
 	}
 }
---
+++ timecode.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func almostEqual(a, b float64, precision int) bool {
+	return (1 / math.Pow10(precision+1)) > math.Abs(a-b)
+}
+
+func gte(a, b float64, precision int) bool {
+	if !almostEqual(a, b, precision) {
+		return a > b
+	}
+	return true
+}
+
+func floatStr(a float64, precision int) string {
+	return strconv.FormatFloat(a, 'f', precision, 64)
+}
+
+func processTimeCodeFile(in, out *os.File, precision int) (valid bool, err error) {
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanLines)
+
+	currentLineNumber := 0
+	var previousTimestamp float64
+	firstTimestamp := true
+
+	valid = true
+
+	var writer *bufio.Writer
+
+	if out != nil {
+		writer = bufio.NewWriter(out)
+		defer func(writer *bufio.Writer) {
+			_ = writer.Flush()
+		}(writer)
+	}
+
+	for scanner.Scan() {
+		l := scanner.Text()
+		if l != "" && !strings.HasPrefix(l, "#") {
+			if ts, err := strconv.ParseFloat(l, 64); err == nil {
+				if !firstTimestamp {
+					if gte(previousTimestamp, ts, precision) {
+						fmt.Printf("%d: not monotone increasing: %s -> %s", currentLineNumber, floatStr(previousTimestamp, precision), floatStr(ts, precision))
+						valid = false
+						if out != nil {
+							fixedTimestamp := previousTimestamp + 1/math.Pow10(precision)
+							fmt.Printf(" changing to %s", floatStr(fixedTimestamp, precision))
+							ts = fixedTimestamp
+							l = floatStr(ts, precision)
+						}
+						fmt.Println("")
+					}
+				}
+				firstTimestamp = false
+				previousTimestamp = ts
+			} else {
+				fmt.Printf("%d: invalid data: %s\n", currentLineNumber, l)
+				l = "## " + l
+			}
+		}
+		if out != nil {
+			_, err := writer.WriteString(l + "\n")
+			if err != nil {
+				return false, nil
+			}
+		}
+
+		currentLineNumber++
+	}
+	return
+}
+
+func CheckTimeCodeFile(in *os.File, precision int) (bool, error) {
+	return processTimeCodeFile(in, nil, precision)
+}
+
+func CleanTimecodeFile(in, out *os.File, precision int) (bool, error) {
+	return processTimeCodeFile(in, out, precision)
+}
